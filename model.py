dbd35481f5b916db7367ef586dc9c70561236ca4
Vyacheslav Matva <vmatva@gmail.com>
2022-09-30 18:01:34 +0300

    Полет шариков.

--- model.py
+++ model.py
@@ -20,14 +20,14 @@ def dec_balls_per_second():
         balls_per_second=0
 
 def add_random_ball():
-    size = random.randint(2, 5)
-    x = random.randint(int(size / 2), 500 - int(size / 2))
-    y = random.randint(int(size / 2), 500 - int(size / 2))
+    size = random.randint(2, 8)
+    x = random.randint(size, 500 - size)
+    y = random.randint(size, 500 - size)
     r = random.randint(30, 255)
     g = random.randint(30, 255)
     b = random.randint(30, 255)
-    speedx = random.randint(1, 4)
-    speedy = random.randint(1, 4)
+    speedx = random.randint(10, 40)/10
+    speedy = random.randint(10, 40)/10
 
     ball = {"x":x, "y":y, "radius":size, "speedx":speedx, "speedy":speedy, "speed_y_fall":0, "color": [r, g, b]}
 
@@ -57,30 +57,30 @@ def step():
 def move_balls():
     for ball in balls:
         ball["x"] += ball["speedx"]
-        if ball["x"]+ball["radius"]/2 > 500:
+        if ball["x"]+ball["radius"] > 500:
             ball["speedx"] = -ball["speedx"]
-            ball["x"] = 500-ball["radius"]/2
+            ball["x"] = 500-ball["radius"]
 
-        if ball["x"]-ball["radius"]/2 < 0:
+        if ball["x"]-ball["radius"] < 0:
             ball["speedx"] = -ball["speedx"]
-            ball["x"] = ball["radius"]/2
+            ball["x"] = ball["radius"]
 
         ball["y"] += ball["speedy"]
-        if ball["y"]+ball["radius"]/2 > 500:
+        if ball["y"]+ball["radius"] > 500:
             ball["speedy"] = -ball["speedy"]
-            ball["y"] = 500-ball["radius"]/2
+            ball["y"] = 500-ball["radius"]
 
-        if ball["y"]-ball["radius"]/2 < 0:
+        if ball["y"]-ball["radius"] < 0:
             ball["speedy"] = -ball["speedy"]
-            ball["y"] = ball["radius"]/2
+            ball["y"] = ball["radius"]
 
 def fall_balls():
     for ball in balls:
         ball["speed_y_fall"]+=0.05
         ball["y"]+=ball["speed_y_fall"]
 
-        if ball["y"]+ball["radius"]/2 > 498:
+        if ball["y"]+ball["radius"] > 500:
             zamedl = 1.6/ball["radius"]
             ball["speed_y_fall"] = -ball["speed_y_fall"]*zamedl
-            ball["y"] = 498 - ball["radius"] / 2
+            ball["y"] = 500 - ball["radius"]
 
